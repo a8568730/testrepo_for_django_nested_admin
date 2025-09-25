f5a7d146cfc2840a3a8cd7539f06881e665c4323
bunli <bunli@ithuan.tw>
2025-09-25 14:14:58 +0800

    * Remove unused CategoryInline in admin.py.

--- test_site/test_app/admin.py
+++ test_site/test_app/admin.py
@@ -1,33 +1,19 @@
 from django.contrib import admin
 import nested_admin
 
-from test_app.models import HeadWord, Pronunciation, \
-    CategoryMapping, Category
+from test_app.models import HeadWord, Pronunciation
 
 
-class PronunciationInline(
-    nested_admin.SortableHiddenMixin,
-    nested_admin.NestedTabularInline,
-):
+class PronunciationInline(nested_admin.NestedTabularInline):
     model = Pronunciation
     fields = ('pronunce', 'order', )
     sortable_field_name = 'order'
-
-
-class CategoryMappingInline(
-    nested_admin.SortableHiddenMixin,
-    nested_admin.NestedTabularInline,
-):
-    model = CategoryMapping
-    fk_name = 'headword'
     classes = ['collapse']
-    sortable_field_name = 'order'
 
 
 class HeadWordAdmin(nested_admin.NestedModelAdmin):
     list_display = ('id', 'word',)
-    inlines = [PronunciationInline, CategoryMappingInline, ]
+    inlines = [PronunciationInline, ]
 
 
 admin.site.register(HeadWord, HeadWordAdmin)
-admin.site.register(Category)
